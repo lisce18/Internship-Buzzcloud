9ee3e9a5af67da386fa3ff659190e9ebd4434223
Wilhelm Lövgren Olofsson <wille@MacBook-Pro.local>
2024-11-27 12:04:02 +0100

    Donse some small fixes to functionality

--- contract/contracts/AnonymousVoting.sol
+++ contract/contracts/AnonymousVoting.sol
@@ -1,10 +1,10 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.27;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
 
-import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract AnonymousVoting{
-    enum VotingState {NotStarted, Ongoing, Finished}
+contract AnonymousVoting {
+    enum VotingState { NotStarted, Ongoing, Finished }
 
     struct Option {
         string name;
@@ -12,96 +12,116 @@ contract AnonymousVoting{
     }
 
     Option[] public options;
+    address[] public voterList;
+    address public owner;
+    string public winner;
     VotingState public votingState;
     mapping(address => bool) public hasVoted;
-    string public winner;
-    address public owner;
     uint256 public votingEnd;
     IERC20 public buzzCoin;
 
-    event VotingStarted(uint256 votingEnd);
-    event VoteCast(address voter, string message);
+    event VoteCast(bytes32 voteHash, string message);
     event VotingFinished(string winner);
+    event VotingStarted(uint256 votingEnd);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function!");
+        _;
+    }
+
+    modifier inState(VotingState _state) {
+        require(votingState == _state, "Invalid state!");
+        _;
+    }
+
+    modifier onlyTokenHolder() {
+        require(buzzCoin.balanceOf(msg.sender) > 0, "Insufficient BuzzCoin to vote!");
+        _;
+    }
 
     constructor(address _buzzCoin) {
         owner = msg.sender;
         buzzCoin = IERC20(_buzzCoin);
         votingState = VotingState.NotStarted;
-        winner = '';
+        winner = "";
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, 'Only owner can call this function!');
-        _;
-    }
+    function startVoting(string[] memory _votingOptions, uint256 _votingDuration) public onlyOwner inState(VotingState.NotStarted) {
+        require(_votingOptions.length > 1, "Must have at least 2 voting options!");
 
-    modifier inState(VotingState _state) {
-        require(votingState == _state, 'Invalid state!');
-        _;
-    }
-
-    modifier onlyTokenHolder() {
-        require(buzzCoin.balanceOf(msg.sender) > 0, 'Must hold BuzzCoin to vote!');
-        _;
-    }
-
-    modifier onlyBeforeEnd() {
-        require(block.timestamp < votingEnd, 'Voting has ended!');
-        _;
-    }
-
-    function startVoting(string[] memory _votingOptions, uint256 _votingDuration) public onlyOwner inState(VotingState.NotStarted){
-        require(_votingOptions.length > 1, 'Must have at least 2 voting options!');
-        for(uint i = 0; i < _votingOptions.length; i++){
-        options.push(Option({name: _votingOptions[i], voteCount: 0}));
+        delete options;
+        for (uint256 i = 0; i < _votingOptions.length; i++) {
+            options.push(Option({ name: _votingOptions[i], voteCount: 0 }));
         }
+
+        delete voterList;
         votingEnd = block.timestamp + _votingDuration;
         votingState = VotingState.Ongoing;
         emit VotingStarted(votingEnd);
     }
 
-    function vote(string memory optionName) public inState(VotingState.Ongoing) onlyTokenHolder onlyBeforeEnd{
-        require(!hasVoted[msg.sender], 'You have already voted!');
+    function vote(string memory optionName) public inState(VotingState.Ongoing) onlyTokenHolder {
+        require(block.timestamp < votingEnd, "Voting has ended!");
+        require(!hasVoted[msg.sender], "You have already voted!");
 
-        bool _found = false;
-        for(uint i = 0; i < options.length; i++){
-            if(keccak256(abi.encodePacked(options[i].name)) == keccak256(abi.encodePacked(optionName))){
+        bool found = false;
+        for (uint256 i = 0; i < options.length; i++) {
+            if (keccak256(abi.encodePacked(options[i].name)) == keccak256(abi.encodePacked(optionName))) {
                 options[i].voteCount += 1;
-                _found = true;
+                found = true;
                 break;
             }
         }
 
-        require(_found, 'Invalid vote option!');
+        require(found, "Invalid vote option!");
 
         hasVoted[msg.sender] = true;
-        keccak256(abi.encodePacked(msg.sender, optionName));
-        emit VoteCast(msg.sender, 'Vote successfully casted!');
+        voterList.push(msg.sender);
+        bytes32 voteHash = keccak256(abi.encodePacked(msg.sender, optionName));
+        emit VoteCast(voteHash, "Vote successfully cast!");
     }
 
-    function finishVoting() public {
-        require(block.timestamp >= votingEnd, 'Voting has not ended yet!');
-        require(votingState == VotingState.Ongoing, 'Voting has not started yet!');
-        votingState = VotingState.Finished;
+    function finishVoting() public onlyOwner {
+    require(block.timestamp >= votingEnd, "Voting is still ongoing!");
 
-        uint256 highestVotes = 0;
-        string memory winningOption;
-        for(uint256 i = 0; i < options.length; i++){
-            if(options[i].voteCount > highestVotes){
-                highestVotes = options[i].voteCount;
-                winningOption = options[i].name;
-            }
+    votingState = VotingState.Finished;
+
+    uint256 highestVotes = 0;
+    string memory _winner = "";
+
+    for (uint256 i = 0; i < options.length; i++) {
+        if (options[i].voteCount > highestVotes) {
+            highestVotes = options[i].voteCount;
+            _winner = options[i].name;
+        } else if (options[i].voteCount == highestVotes) {
+            _winner = "Tie";
         }
-        winner = winningOption;
-        emit VotingFinished(winner);
     }
 
-    function getWinner() public view returns (string memory){
-        require(votingState == VotingState.Finished, 'Voting has not ended yet!');
+    winner = _winner;
+    emit VotingFinished(winner);
+}
+
+    function resetVoting() public onlyOwner inState(VotingState.Finished) {
+    votingState = VotingState.NotStarted;
+    winner = "";
+
+    delete options;
+
+    for (uint256 i = 0; i < voterList.length; i++) {
+        hasVoted[voterList[i]] = false;
+    }
+
+    delete voterList;
+}
+
+
+    function getWinner() public view returns (string memory) {
+        require(votingState == VotingState.Finished, "Voting has not ended yet!");
         return winner;
     }
 
-    function getOptionCount () public view returns (uint256){
+    function getOptionCount() public view returns (uint256) {
         return options.length;
     }
 }
--- contract/test/AnonymousVoting.ts
+++ contract/test/AnonymousVoting.ts
@@ -1,5 +1,4 @@
 import { expect } from "chai";
-import { AbiCoder } from "ethers";
 import hre, { ethers } from "hardhat";
 
 describe("AnonymousVoting", function () {
@@ -36,42 +35,41 @@ describe("AnonymousVoting", function () {
             it("Should allow the owner to start the voting", async function () {
                 const { contract, owner } = await deployContractFixture();
 
-                const options = ["Option1", "Option2", "Option3"];
-
                 await expect(
-                    contract.connect(owner).startVoting(options, 3600)
+                    contract
+                        .connect(owner)
+                        .startVoting(["Option 1", "Option 2"], 60)
                 ).to.emit(contract, "VotingStarted");
             });
 
             it("Should not allow non-owner to start voting", async function () {
                 const { contract, addr1 } = await deployContractFixture();
 
-                const options = ["Option1", "Option2", "Option3"];
-
                 await expect(
-                    contract.connect(addr1).startVoting(options, 3600)
-                ).to.be.revertedWith("Only owner can call this function!");
+                    contract
+                        .connect(addr1)
+                        .startVoting(["Option 1", "Option 2"], 60)
+                ).to.be.revertedWith("Only the owner can call this function!");
             });
 
             it("Should not allow owner to start a voting when one is already started", async function () {
                 const { contract, owner } = await deployContractFixture();
 
                 const options = ["Option1", "Option2", "Option3"];
-
                 await contract.connect(owner).startVoting(options, 3600);
 
                 await expect(
-                    contract.connect(owner).startVoting(options, 3600)
+                    contract
+                        .connect(owner)
+                        .startVoting(["Option 1", "Option 2"], 60)
                 ).to.be.revertedWith("Invalid state!");
             });
 
             it("Should not allow starting a voting with less than 2 options", async function () {
                 const { contract, owner } = await deployContractFixture();
 
-                const options = ["Option1"];
-
                 await expect(
-                    contract.connect(owner).startVoting(options, 3600)
+                    contract.connect(owner).startVoting(["Option 1"], 60)
                 ).to.be.revertedWith("Must have at least 2 voting options!");
             });
 
@@ -80,8 +78,8 @@ describe("AnonymousVoting", function () {
 
                 const options = ["Option1", "Option2", "Option3"];
 
-                await expect(
-                    contract.connect(owner).startVoting(options, 3600)
+                expect(
+                    await contract.connect(owner).startVoting(options, 3600)
                 ).to.emit(contract, "VotingStarted");
             });
         });
@@ -108,14 +106,17 @@ describe("AnonymousVoting", function () {
 
                 await contract.connect(owner).startVoting(options, 3600);
 
-                const amount = ethers.parseUnits("100", 18);
+                const amount = ethers.parseEther("1");
                 await buzzCoin.transfer(addr1.address, amount);
                 await buzzCoin.transfer(addr2.address, amount);
                 await buzzCoin.transfer(addr3.address, amount);
+                await buzzCoin
+                    .connect(addr3)
+                    .approve(contract.getAddress(), amount);
 
-                await contract.connect(addr1).vote(options[0]);
-                await contract.connect(addr2).vote(options[1]);
-                await contract.connect(addr3).vote(options[1]);
+                await contract.connect(addr1).vote("Option1");
+                await contract.connect(addr2).vote("Option2");
+                await contract.connect(addr3).vote("Option2");
 
                 await ethers.provider.send("evm_increaseTime", [3601]);
                 await ethers.provider.send("evm_mine", []);
@@ -125,7 +126,7 @@ describe("AnonymousVoting", function () {
                 expect(winner).to.equal("Option2");
             });
 
-            it("Should not allow finishing the vote before the voting period has ended", async function () {
+            it("Should not finish voting if time has not ended", async function () {
                 const { contract, owner } = await deployContractFixture();
 
                 const options = ["Option1", "Option2", "Option3"];
@@ -133,7 +134,7 @@ describe("AnonymousVoting", function () {
                 await contract.connect(owner).startVoting(options, 3600);
 
                 await expect(contract.finishVoting()).to.be.revertedWith(
-                    "Voting has not ended yet!"
+                    "Voting is still ongoing!"
                 );
             });
 
@@ -141,8 +142,45 @@ describe("AnonymousVoting", function () {
                 const { contract } = await deployContractFixture();
 
                 await expect(contract.finishVoting()).to.be.revertedWith(
-                    "Voting has not started yet!"
+                    "Invalid state!"
                 );
+            });
+
+            it("Should do nothing if voting is already finished", async function () {
+                const { buzzCoin, contract, owner, addr1 } =
+                    await deployContractFixture();
+
+                const options = ["Option1", "Option2", "Option3"];
+
+                await contract.connect(owner).startVoting(options, 3600);
+
+                const amount = ethers.parseEther("1");
+                await buzzCoin.transfer(addr1.address, amount);
+
+                await contract.connect(addr1).vote("Option1");
+
+                await ethers.provider.send("evm_increaseTime", [3601]);
+                await ethers.provider.send("evm_mine", []);
+
+                await contract.finishVoting();
+
+                expect(await contract.votingState()).to.equal(2);
+            });
+
+            it("Should handle no votes cast and finish voting gracefully", async function () {
+                const { contract, owner } = await deployContractFixture();
+
+                await contract
+                    .connect(owner)
+                    .startVoting(["Option1", "Option2"], 3600);
+
+                await ethers.provider.send("evm_increaseTime", [3601]);
+                await ethers.provider.send("evm_mine", []);
+
+                await contract.finishVoting();
+
+                expect(await contract.votingState()).to.equal(2);
+                expect(await contract.getWinner()).to.equal("Tie");
             });
         });
 
@@ -155,17 +193,17 @@ describe("AnonymousVoting", function () {
 
                 await contract.connect(owner).startVoting(options, 3600);
 
-                const amount = ethers.parseUnits("100", 18);
+                const amount = ethers.parseEther("1");
                 await buzzCoin.transfer(addr1.address, amount);
 
-                expect(await contract.connect(addr1).vote(options[0])).to.emit(
+                expect(await contract.connect(addr1).vote("Option1")).to.emit(
                     contract,
                     "VoteCast"
                 );
             });
 
             it("Should not allow non-token holders to vote", async function () {
-                const { contract, owner, addr1 } =
+                const { buzzCoin, contract, owner, addr1 } =
                     await deployContractFixture();
 
                 const options = ["Option1", "Option2", "Option3"];
@@ -173,8 +211,8 @@ describe("AnonymousVoting", function () {
                 await contract.connect(owner).startVoting(options, 3600);
 
                 await expect(
-                    contract.connect(addr1).vote(options[0])
-                ).to.be.revertedWith("Must hold BuzzCoin to vote!");
+                    contract.connect(addr1).vote("Option1")
+                ).to.be.revertedWith("Insufficient BuzzCoin to vote!");
             });
 
             it("Should not allow voting twice", async function () {
@@ -182,17 +220,46 @@ describe("AnonymousVoting", function () {
                     await deployContractFixture();
 
                 const options = ["Option1", "Option2", "Option3"];
-
                 await contract.connect(owner).startVoting(options, 3600);
 
-                const amount = ethers.parseUnits("100", 18);
+                const amount = ethers.parseEther("2");
                 await buzzCoin.transfer(addr1.address, amount);
 
-                await contract.connect(addr1).vote(options[0]);
+                await contract.connect(addr1).vote("Option1");
 
                 await expect(
-                    contract.connect(addr1).vote(options[0])
+                    contract.connect(addr1).vote("Option1")
                 ).to.be.revertedWith("You have already voted!");
+            });
+
+            it("Should allow users to vote again when a new vote is started", async function () {
+                const { buzzCoin, contract, owner, addr1 } =
+                    await deployContractFixture();
+
+                await contract
+                    .connect(owner)
+                    .startVoting(["Option1", "Option2"], 3600);
+
+                const amount = ethers.parseEther("1");
+                await buzzCoin.transfer(addr1.address, amount);
+
+                await contract.connect(addr1).vote("Option1");
+
+                await ethers.provider.send("evm_increaseTime", [3601]);
+                await ethers.provider.send("evm_mine", []);
+                await contract.finishVoting();
+                await contract.connect(owner).resetVoting();
+
+                expect(await contract.hasVoted(addr1.address)).to.equal(false);
+
+                await contract
+                    .connect(owner)
+                    .startVoting(["Option1", "Option2"], 3600);
+
+                await expect(contract.connect(addr1).vote("Option2")).to.emit(
+                    contract,
+                    "VoteCast"
+                );
             });
 
             it("Should not allow voting with an invalid option", async function () {
@@ -200,10 +267,9 @@ describe("AnonymousVoting", function () {
                     await deployContractFixture();
 
                 const options = ["Option1", "Option2", "Option3"];
-
                 await contract.connect(owner).startVoting(options, 3600);
 
-                const amount = ethers.parseUnits("100", 18);
+                const amount = ethers.parseEther("1");
                 await buzzCoin.transfer(addr1.address, amount);
 
                 const invalidOption =
@@ -219,18 +285,123 @@ describe("AnonymousVoting", function () {
                     await deployContractFixture();
 
                 const options = ["Option1", "Option2", "Option3"];
-
                 await contract.connect(owner).startVoting(options, 3600);
 
-                const amount = ethers.parseUnits("100", 18);
+                const amount = ethers.parseEther("1");
                 await buzzCoin.transfer(addr1.address, amount);
 
                 await ethers.provider.send("evm_increaseTime", [3601]);
                 await ethers.provider.send("evm_mine", []);
 
                 await expect(
-                    contract.connect(addr1).vote(options[0])
+                    contract.connect(addr1).vote("Option1")
                 ).to.be.revertedWith("Voting has ended!");
+            });
+
+            it("Should revert if a voter tries to vote with an empty option", async function () {
+                const { buzzCoin, contract, owner, addr1 } =
+                    await deployContractFixture();
+
+                const options = ["Option1", "Option2", "Option3"];
+                await contract.connect(owner).startVoting(options, 3600);
+
+                const amount = ethers.parseEther("1");
+                await buzzCoin.transfer(addr1.address, amount);
+
+                await expect(
+                    contract.connect(addr1).vote("")
+                ).to.be.revertedWith("Invalid vote option!");
+            });
+        });
+
+        describe("Reset Voting", function () {
+            it("should reset voting correctly", async function () {
+                const { buzzCoin, contract, owner, addr1, addr2 } =
+                    await deployContractFixture();
+
+                await contract
+                    .connect(owner)
+                    .startVoting(["Option 1", "Option 2"], 3600);
+
+                const amount = ethers.parseEther("1");
+                await buzzCoin.transfer(addr1.address, amount);
+                await buzzCoin.transfer(addr2.address, amount);
+
+                await contract.connect(addr1).vote("Option 1");
+                await contract.connect(addr2).vote("Option 2");
+
+                await ethers.provider.send("evm_increaseTime", [3602]);
+                await ethers.provider.send("evm_mine", []);
+
+                await contract.connect(owner).finishVoting();
+
+                await contract.connect(owner).resetVoting();
+
+                expect(await contract.votingState()).to.equal(0);
+                expect(await contract.getOptionCount()).to.equal(0);
+                expect(await contract.hasVoted(addr1.address)).to.equal(false);
+                expect(await contract.hasVoted(addr2.address)).to.equal(false);
+            });
+
+            it("Should reset the contracts state after voting ends", async function () {
+                const { buzzCoin, contract, owner, addr1 } =
+                    await deployContractFixture();
+
+                const options = ["Option1", "Option2", "Option3"];
+                await contract.connect(owner).startVoting(options, 3600);
+
+                const amount = ethers.parseEther("1");
+                await buzzCoin.transfer(addr1.address, amount);
+
+                await contract.connect(addr1).vote("Option1");
+
+                await ethers.provider.send("evm_increaseTime", [3601]);
+                await ethers.provider.send("evm_mine", []);
+                await contract.finishVoting();
+
+                expect(await contract.votingState()).to.equal(2);
+                expect(await contract.getWinner()).to.equal("Option1");
+
+                await contract.resetVoting();
+
+                expect(await contract.votingState()).to.equal(0);
+                expect(await contract.getOptionCount()).to.equal(0);
+            });
+
+            it("Should allow resetting the contract immediately after finishing voting", async function () {
+                const { buzzCoin, contract, owner, addr1 } =
+                    await deployContractFixture();
+
+                await contract
+                    .connect(owner)
+                    .startVoting(["Option1", "Option2"], 3600);
+
+                const amount = ethers.parseEther("1");
+
+                await buzzCoin.transfer(addr1.address, amount);
+                await contract.connect(addr1).vote("Option1");
+
+                await ethers.provider.send("evm_increaseTime", [3601]);
+                await ethers.provider.send("evm_mine", []);
+                await contract.finishVoting();
+
+                expect(await contract.votingState()).to.equal(2);
+
+                await contract.resetVoting();
+
+                expect(await contract.votingState()).to.equal(0);
+                expect(await contract.getOptionCount()).to.equal(0);
+            });
+
+            it("Should not allow resetting the vote while it is ongoing", async function () {
+                const { contract, owner } = await deployContractFixture();
+
+                const options = ["Option1", "Option2", "Option3"];
+                await contract.connect(owner).startVoting(options, 3600);
+
+                await expect(contract.resetVoting()).to.be.revertedWith(
+                    "Invalid state!"
+                );
             });
         });
 
@@ -242,6 +413,27 @@ describe("AnonymousVoting", function () {
                     "Voting has not ended yet!"
                 );
             });
+
+            it("Should present winner when vote is over", async function () {
+                const { buzzCoin, contract, owner, addr1 } =
+                    await deployContractFixture();
+
+                const options = ["Option1", "Option2", "Option3"];
+                await contract.connect(owner).startVoting(options, 3600);
+
+                const amount = ethers.parseEther("1");
+                await buzzCoin.transfer(addr1.address, amount);
+
+                await contract.connect(addr1).vote("Option1");
+
+                await ethers.provider.send("evm_increaseTime", [3601]);
+                await ethers.provider.send("evm_mine", []);
+
+                await contract.finishVoting();
+
+                const winner = await contract.getWinner();
+                expect(winner).to.equal("Option1");
+            });
         });
 
         describe("Get Option Count", function () {
@@ -249,10 +441,34 @@ describe("AnonymousVoting", function () {
                 const { contract, owner } = await deployContractFixture();
 
                 const options = ["Option1", "Option2", "Option3"];
-
                 await contract.connect(owner).startVoting(options, 3600);
 
-                expect(await contract.getOptionCount()).to.equal(3);
+                const optionCount = await contract.getOptionCount();
+                expect(optionCount).to.equal(3);
+            });
+
+            it("Should return 0 if no vote is ongoing", async function () {
+                const { contract } = await deployContractFixture();
+
+                const optionCount = await contract.getOptionCount();
+                expect(optionCount).to.equal(0);
+            });
+
+            it("Should reset the option count after resetting the vote", async function () {
+                const { contract, owner } = await deployContractFixture();
+
+                const options = ["Option1", "Option2", "Option3"];
+                await contract.connect(owner).startVoting(options, 3600);
+
+                await ethers.provider.send("evm_increaseTime", [3601]);
+                await ethers.provider.send("evm_mine", []);
+
+                await contract.finishVoting();
+
+                await contract.resetVoting();
+
+                const optionCount = await contract.getOptionCount();
+                expect(optionCount).to.equal(0);
             });
         });
     });
